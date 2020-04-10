e12a05bdefca3fd1e47ea9dc464ed9a3fbb70c01
bilalfareed83 <bilal_fareed@hotmail.com>
2020-04-10 18:55:32 +0500

    first commit

--- App.js
+++ App.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import axios from 'axios';
+import { TextInput, Appbar, Button, List } from 'react-native-paper';
 
 export default class App extends React.Component {
   state = {
-    text: 'Loading...',
+    text: '',
+    todos: [],
   };
 
-  componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
-      console.log(res);
-      this.setState({ text: res.data[0].name });
-    });
-  }
+  add = () => {
+    if (this.state.text) {
+      let margedTodo = [...this.state.todos, this.state.text];
+
+      console.log('add function working');
+      this.setState({ todos: margedTodo, text: '' });
+    } else {
+      alert('please enter some text');
+    }
+  };
+
+  // componentDidMount() {}
   render() {
+    console.log(this.state.todos);
     return (
       <View style={styles.container}>
-        <Text>{this.state.text}</Text>
-        <Text>React Native Practice</Text>
+        <Appbar.Header>
+          <Appbar.Content title="Todos App" style={{ alignItems: 'center' }} />
+        </Appbar.Header>
+        <TextInput
+          label="Todo"
+          placeholder="Add todo"
+          value={this.state.text}
+          onChangeText={(text) => this.setState({ text })}
+        />
+
+        <Button
+          icon="heart"
+          mode="contained"
+          onPress={this.add}
+          style={{ margin: 10 }}
+        >
+          Add Todo
+        </Button>
+
+        {this.state.todos.map((el, i) => {
+          return (
+            <List.Item
+              title={el}
+              description="Item description"
+              left={(props) => <List.Icon {...props} icon="folder" />}
+              onPress={() => {
+                this.state.todos.splice(i, 1);
+                this.setState({ todos: this.state.todos });
+              }}
+              id={i}
+              key={i}
+            />
+          );
+        })}
       </View>
     );
   }
@@ -27,7 +67,7 @@ const styles = StyleSheet.create({
     // flex: 1,
     marginTop: 50,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
